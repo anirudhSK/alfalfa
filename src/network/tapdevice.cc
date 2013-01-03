d922217506fc5fcf5be996cd81a009c525311a17
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-01-03 16:51:07 -0500

    Pruned out comments and added acks.

--- src/network/tapdevice.cc
+++ src/network/tapdevice.cc
@@ -1,5 +1,7 @@
 #include "tapdevice.hh"
 
+/* Borrowed heavily from http://backreference.org/2010/03/26/tuntap-interface-tutorial/ */
+
 int tun_alloc(char *dev, int flags) {
   struct ifreq ifr;
   int fd;
@@ -11,11 +13,11 @@ int tun_alloc(char *dev, int flags) {
   	return fd;
   }
   
-  /* preparation of the struct ifr, of type "struct ifreq" */
+  /* zero out struct ifr, of type "struct ifreq" */
   memset(&ifr, 0, sizeof(ifr));
-  ifr.ifr_flags = flags;   /* IFF_TUN or IFF_TAP, plus maybe IFF_NO_PI */
+  ifr.ifr_flags = flags;   /* IFF_TUN or IFF_TAP, optionally IFF_NO_PI */
   
-  /* if a device name was specified, put it in the structure */
+  /* if a device name is supplied, use it */
   if (*dev) {
   	strncpy(ifr.ifr_name, dev, IFNAMSIZ);
   }
@@ -26,9 +28,7 @@ int tun_alloc(char *dev, int flags) {
   	return err;
   }
   
-  /* if the operation was successful, write back the name of the
-   * interface to the variable "dev", so the caller can know
-   * it. Note that the caller MUST reserve space in *dev */
+  /* if successful, write back device name to the variable "dev" */
   strcpy(dev, ifr.ifr_name);
   
   /* return file descriptor to tap device */
