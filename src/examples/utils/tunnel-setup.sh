eaefc1f8add07a4f383ffb151f34297b27e164ee
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-01-05 12:45:10 -0500

    More setup instructions

--- src/examples/utils/tunnel-setup.sh
+++ src/examples/utils/tunnel-setup.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/bash
 
+# pre-requisite:
+#Set up tethering on the desktop using the donat script.
+#Ensure tethered interface is called usb0 since that's been hardcoded.
+
 # On relay :
 sudo tunctl -t tap0 -u user_name
 sudo ifconfig tap0 10.0.0.2 netmask 255.255.255.0 up
@@ -18,7 +22,10 @@ sudo iptables -F
 # setup
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT -i eth0 -o tap0
-sudo iptables -A FORWARD -j ACCEPT -o rmnet0 -i rndis0
+sudo iptables -A FORWARD -j ACCEPT -o eth0 -i tap0
+# enable forwarding
+sudo su
+echo 1 > /proc/sys/net/ipv4/ip_forward
 
 # Now run sprout :
 # On relay :
