5e6efcf93bf59b22a1c953da029b317e59c1f614
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-01-10 14:53:33 -0500

    Reduce MTU to 1420 to prevent fragmentation.

--- src/examples/sprout-client.sh
+++ src/examples/sprout-client.sh
@@ -10,5 +10,6 @@ relay_port=$2
 set -x
 sudo tunctl -t tap0
 sudo ifconfig tap0 10.0.0.1 netmask 255.255.255.0 up
+sudo ifconfig tap0 mtu 1420
 sudo route add default gw 10.0.0.2 metric 2 # ensure eth0 is preferred to tap0
 sudo ./sproutbt2 $relay_address $relay_port
--- src/examples/sprout-relay.sh
+++ src/examples/sprout-relay.sh
@@ -3,4 +3,5 @@
 set -x
 sudo tunctl -t tap0
 sudo ifconfig tap0 10.0.0.2 netmask 255.255.255.0 up
+sudo ifconfig tap0 mtu 1420
 sudo ./sproutbt2 # binds to eth0 and tap0
