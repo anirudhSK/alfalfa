e6cf4d4639f9d162ab49ca3097c49f587b3eefb4
skype-beta <skype-beta@ThinkCentre-beta.(none)>
2013-02-11 16:03:09 -0500

    Go back to much simpler earlier version 2e676a0b4a5ff8dcb422c3efafceed453330b67f

--- src/examples/sproutbt2.cc
+++ src/examples/sproutbt2.cc
@@ -3,12 +3,10 @@
 #include <assert.h>
 #include <list>
 
-#include "packet-fragment.pb.h"
 #include "sproutconn.h"
 #include "select.h"
 #include <sys/time.h>
 #include "tapdevice.hh"
-#include "reassembly.hh"
 
 using namespace std;
 using namespace Network;
@@ -26,8 +24,7 @@ int main( int argc, char *argv[] )
   int tap_fd = setup_tap();
 
   /* Queue incoming packets from tap0 */
-  uint32_t count = 0;
-  std::queue<fragment::PacketFragment> ingress_queue;
+  std::queue<string> ingress_queue;
 
   if ( argc > 1 ) {
     /* client */
@@ -48,7 +45,7 @@ int main( int argc, char *argv[] )
   sel.add_fd( net->fd() );
   sel.add_fd( tap_fd );
 
-  const int fallback_interval = 10;
+  const int fallback_interval = 50;
 
   /* wait to get attached */
   if ( server ) {
@@ -71,75 +68,30 @@ int main( int argc, char *argv[] )
 
   uint64_t time_of_next_transmission = timestamp() + fallback_interval;
 
-  fprintf( stderr, "Looping...\n" );
+  fprintf( stderr, "Looping...\n" );  
   unsigned long int cumulative_bytes = 0;
-
-  /* book keeping for reassembly */
-  Reassembly reassembler;
 
   /* loop */
   while ( 1 ) {
-    uint32_t bytes_to_send = net->window_size();
+    int bytes_to_send = net->window_size();
 
-    /* Action 1 : actually send, maybe */
+    /* actually send, maybe */
     if ( ( bytes_to_send > 0 ) || ( time_of_next_transmission <= timestamp() ) ) {
       do {
-        if ( !ingress_queue.empty() ) {
-          /* Queue has packets */
-          /* Get HOL packet fragment */
-          fragment::PacketFragment par_pkt = ingress_queue.front();
-
-          if (bytes_to_send == 0) {
-            /* Check "window" i.e. bytes_to_send is empty, simply send fallback packet */
-            net->send( string(0,'x'), fallback_interval );
-            break;
-
-          } else if ( bytes_to_send < par_pkt.payload().size() ) {
-            /* Window is not zero, but we don't have sufficient bytes to send HOL packet */
-            /* Fragment par_pkt into 2 packets : a new "to_send" pkt and a modified HOL pkt */
-
-            /* new to_send */
-            fragment::PacketFragment to_send ;
-            to_send.set_id( par_pkt.id() );         /* same id on all fragments */
-            to_send.set_length( par_pkt.length() ); /* same length on all fragments */
-            to_send.set_payload( par_pkt.payload().substr( 0, bytes_to_send ) );/* fragment */
-            to_send.set_offset( par_pkt.offset() ); /* offset on first fragment */
-            to_send.set_more_frags( true );         /* Definitely set more_frags */
-            net->send( to_send.SerializeAsString(), fallback_interval );
-
-            /* modified HOL packet */
-            ingress_queue.front().set_offset( par_pkt.offset() + bytes_to_send );
-            ingress_queue.front().set_payload( par_pkt.payload().substr( bytes_to_send, par_pkt.payload().size() - bytes_to_send ) );
-
-	    fprintf( stderr, "SENDING snipped packet, seqnum %u of size %lu, leftover %lu \n",
-                     to_send.id(), to_send.payload().size(), par_pkt.payload().size() - bytes_to_send );
-	    break;
-
-	  } else {
-            /* Window has sufficient bytes to send the whole packet, loop around to get more if possible */
-            bytes_to_send -= par_pkt.payload().size();
-            assert( bytes_to_send  >= 0 );
-
-            /* By default, the next packet will follow this one immediately */
-            int time_to_next = 0;
-
-            /* But if our window is exhausted, or queue is empty, fallback to fallback_interval */
-            if ( bytes_to_send == 0 || ingress_queue.empty() ) {
-              time_to_next = fallback_interval;
-	    }
-
-            /* In any case, send current packet after clearing more_frags */
-            par_pkt.set_more_frags( false ); /* clear the more fragments flag on the last packet */
-	    net->send( par_pkt.SerializeAsString(), time_to_next );
-	    fprintf( stderr, "SENDING whole packet of seqnum %u, size %lu \n", par_pkt.id(), par_pkt.payload().size() );
-	    ingress_queue.pop();
+	if ( !ingress_queue.empty() ) {
+	  string packet( ingress_queue.front() );
+          assert( bytes_to_send > 0 );
+	  bytes_to_send -= packet.size(); /* Allow overdraft */
+	  int time_to_next = 0;
+	  if ( bytes_to_send <= 0 || ingress_queue.empty() ) {
+	  	time_to_next = fallback_interval;
 	  }
-
-        } else {
-          /* Queue is empty, simply send fallback packet and break */
+	  net->send( packet, time_to_next );
+	  ingress_queue.pop();
+        }
+        else {
 	  net->send( string(0,'x'), fallback_interval );
 	  bytes_to_send = 0; /* Wasted forecast */
-          break;
         }
       } while ( bytes_to_send > 0 );
 
@@ -147,7 +99,7 @@ int main( int argc, char *argv[] )
 					    time_of_next_transmission );
     }
 
-    /* Action 2 : Calculate wait time  */
+    /* wait */
     int wait_time = time_of_next_transmission - timestamp();
     if ( wait_time < 0 ) {
       wait_time = 0;
@@ -161,46 +113,25 @@ int main( int argc, char *argv[] )
       exit( 1 );
     }
 
-    /* Action 3 : receive on network */
+    /* receive */
     struct timeval tv;
     if ( sel.read( net->fd() ) ) {
       string packet( net->recv() );
-      if (packet.size() == 0 ) {
-          continue;
-      }
 
-      fragment::PacketFragment rx;
-      rx.ParseFromString( packet );
+      /* write into tap0 */
+      write( tap_fd, packet.c_str(), packet.size() );
 
-      /* Add to reassembler */
-      reassembler.add_fragment( rx );
-
-      /* Is reassembly over ? */
-      std::string assembled_packet = reassembler.ready_to_deliver( rx.id() );
-
-      if (  assembled_packet != "" ) {
-        fprintf( stderr, "REASSEMBLED Packet with seqnum %u with length %u \n", rx.id(), rx.length() );
-        if ( write( tap_fd, assembled_packet.c_str(), rx.length() ) < 0 ) {
-          perror( "tap device ");
-          exit( 1 );
-        }
-        gettimeofday( &tv, NULL );
-        cumulative_bytes += packet.size();
-      }
+      gettimeofday( &tv, NULL );
+      cumulative_bytes += packet.size();
+      fprintf( stderr, "Rx packet : %lu bytes, cumulative : %lu bytes at time %f \n", packet.size(), cumulative_bytes, (double) tv.tv_sec+tv.tv_usec/1.0e6 );
     }
 
-    /* Action 4: read from tap0 */
+    /* read from tap0 */
     if ( sel.read( tap_fd ) ) {
       char buffer[1600];
       int nread = read( tap_fd, (void*) buffer, sizeof(buffer) );
       string packet( buffer, nread );
-      fragment::PacketFragment par_pkt;
-      par_pkt.set_length( packet.size() ); /* length of original datagram */
-      par_pkt.set_id( count++ ); /* sequence number of original  datagram */
-      par_pkt.set_offset( 0 );   /* offset is 0 to begin with */
-      par_pkt.set_more_frags( true ); /* copied from IPv4 */
-      par_pkt.set_payload( packet ); /* The actual data */
-      ingress_queue.push( par_pkt );
+      ingress_queue.push( packet );
     }
   }
 }
